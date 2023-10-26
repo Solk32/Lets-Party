a4d6137533bc61bce6021ada45aaa18c104ca8e3
Solk32 <nesolk13@gmail.com>
2023-10-26 15:40:13 +0300

    change listHandler

--- container.go
+++ container.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"net/http"
 	"partyinvites/db"
 )
@@ -24,7 +25,60 @@ func (c *Container) welcomeHandler(writer http.ResponseWriter, request *http.Req
 	templates["welcome"].Execute(writer, nil)
 }
 
+//func (c *Container) listHandler(writer http.ResponseWriter, request *http.Request) {
+//	templates["list"].Execute(writer, responses)
+//	//queryTrue := "SELECT id, name FROM guests WHERE userjoin = true"
+//	//var err error
+//	//_, err = c.DB.Exec(queryTrue)
+//	//if err != nil {
+//	//	panic(err)
+//	//}
+//	//
+//	//fmt.Println("Значение userjoin = true:")
+//	//rowsTrue, err := db.ConnectDB(queryTrue)
+//	//for rowsTrue.Next() {
+//	//	var id int
+//	//	var name string
+//	//	if err := rowsTrue.Scan(&id, &name); err != nil {
+//	//		fmt.Println("Ошибка при сканировании строки:", err)
+//	//		return
+//	//	}
+//	//	fmt.Printf("ID: %d, Name: %s\n", id, name)
+//	//}
+//}
+
 func (c *Container) listHandler(writer http.ResponseWriter, request *http.Request) {
+	queryTrue := "SELECT name, email, phone FROM guests WHERE joinuser = true"
+
+	rowsTrue, err := c.DB.Query(queryTrue)
+	if err != nil {
+		panic(err)
+	}
+	defer rowsTrue.Close()
+
+	var trueGuests []struct {
+		Name  string
+		Email string
+		Phone string
+	}
+
+	for rowsTrue.Next() {
+		var guest struct {
+			Name  string
+			Email string
+			Phone string
+		}
+		if err := rowsTrue.Scan(&guest.Name, &guest.Email, &guest.Phone); err != nil {
+			fmt.Println("Ошибка при сканировании строки:", err)
+			return
+		}
+		trueGuests = append(trueGuests, guest)
+	}
+
+	for _, guest := range trueGuests {
+		fmt.Printf("Name: %s, Email: %s, Phone: %s\n", guest.Name, guest.Email, guest.Phone)
+	}
+
 	templates["list"].Execute(writer, responses)
 }
 
@@ -35,7 +89,7 @@ func (c *Container) formHandler(writer http.ResponseWriter, request *http.Reques
 		})
 	} else if request.Method == http.MethodPost {
 		request.ParseForm()
-		c.CreateGuest(request.Form["name"][0], request.Form["email"][0], request.Form["phone"][0], request.Form["willattend"][0])
+		c.CreateGuest(request.Form["name"][0], request.Form["email"][0], request.Form["phone"][0], request.Form["willattend"][0] == "true")
 		responseData := Party{
 			Name:       request.Form["name"][0],
 			Email:      request.Form["email"][0],
@@ -68,11 +122,11 @@ func (c *Container) formHandler(writer http.ResponseWriter, request *http.Reques
 	}
 }
 
-func (c *Container) CreateGuest(name string, email string, phone string, join string) {
-	sqlStatement := `INSERT INTO guests (name, email, phone, userjoin) 
+func (c *Container) CreateGuest(name string, email string, phone string, userjoin bool) {
+	sqlStatement := `INSERT INTO guests (name, email, phone, joinuser) 
 VALUES ($1, $2, $3, $4)`
 	var err error
-	_, err = c.DB.Exec(sqlStatement, name, email, phone, join)
+	_, err = c.DB.Exec(sqlStatement, name, email, phone, userjoin)
 	if err != nil {
 		panic(err)
 	}
